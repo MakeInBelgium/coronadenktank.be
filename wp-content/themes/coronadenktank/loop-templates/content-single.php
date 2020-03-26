8586baa5fe20a93d8e463b3c5e0c44f22adef625
Bram Esposito <bram@bramesposito.com>
2020-03-26 21:58:44 +0100

    first version of initiative detail page

---
+++ wp-content/themes/coronadenktank/loop-templates/content-single.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Single post partial template
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->

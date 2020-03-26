bc22c37ae8dfa477cb507c73467520143dec67bc
Bram Esposito <bram@bramesposito.com>
2020-03-26 10:12:31 +0100

    theming of excerpt

--- wp-content/themes/coronadenktank/template-parts/blocks/initiatives/initiatives.php
+++ wp-content/themes/coronadenktank/template-parts/blocks/initiatives/initiatives.php
@@ -49,7 +49,7 @@ switch ($v) {
       <div class="initiative">
         <div class="date">{{date}}</div>
         <h4><a href="{{link}}">{{title}}</a></h4>
-         <p>{{excerpt}}</p>
+         <p class="excerpt">{{excerpt}}</p>
         <a href="{{link}}" class="link">{{readmore}}</a>
       </div>
     </div>
@@ -61,7 +61,7 @@ TEMPLATE;
       <div class="initiative">
         <div class="date">{{date}}</div>
         <h4><a href="{{link}}">{{title}}</a></h4>
-         <p>{{excerpt}}</p>
+         <p class="excerpt">{{excerpt}}</p>
         <a href="{{link}}" class="link">{{readmore}}</a>
       </div>
     </div>

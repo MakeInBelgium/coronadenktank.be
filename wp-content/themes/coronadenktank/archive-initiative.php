f4641fec5ad088d24a5320389940fac4dce13508
Bram Esposito <bram@bramesposito.com>
2020-03-26 09:47:39 +0100

    layout fix on archive page

--- wp-content/themes/coronadenktank/archive-initiative.php
+++ wp-content/themes/coronadenktank/archive-initiative.php
@@ -19,7 +19,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+		<div class="">
 
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>

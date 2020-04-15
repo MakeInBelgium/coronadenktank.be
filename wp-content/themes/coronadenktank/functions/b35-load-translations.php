caa171383e21fca08f5d249204efecc90e09921a
Bram Esposito <bram@bramesposito.com>
2020-04-15 15:44:49 +0200

    fix translations of CPT slug
    to be seen if this is robust when translating the site

--- wp-content/themes/coronadenktank/functions/b35-load-translations.php
+++ wp-content/themes/coronadenktank/functions/b35-load-translations.php
@@ -1,2 +1,16 @@
 <?php
+// initiialize some strings:
+$initiative = __("Initiatives", "coronadenktank");
+$initiative_slug = __("initiative", "coronadenktank");
+
 load_theme_textdomain('coronadenktank',get_stylesheet_directory() . '/languages' );
+
+add_filter( 'register_post_type_args', 'wpse247328_register_post_type_args', 10, 2 );
+function wpse247328_register_post_type_args( $args, $post_type ) {
+
+  if ( 'initiative' === $post_type ) {
+    $args['rewrite']['slug'] = __('initiative', "coronadenktank");
+  }
+
+  return $args;
+}
